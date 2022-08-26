0978d0bccda17f065fe8d0213e3b3e61ed65b5ef
templar-command0 <templar.command0@gmail.com>
2022-08-26 19:33:17 +0530

    to get first string and it's end

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # code-formatter
+
+# REGEX
+
+1. match boundary  - /\bm/ matches m in moon , and m in doom
+2. backspace - [\b]
--- script.js
+++ script.js
@@ -11,14 +11,33 @@ const check = {
     });
     return ret ? word + " " : word;
   },
+  string: function (string) {
+    const firstQuoteIndex = getFirstQuoteIndex(string)
+    const firstQuoteType = string[firstQuoteIndex]
+    const firstQuote = {
+      firstQuoteType: firstQuoteType,
+      firstQuoteIndex: firstQuoteIndex
+    }
+    console.log(`firstQuote`, firstQuote);
+    const endOfFirstQuoteIndex = getFirstQuoteIndex((string.slice(firstQuoteIndex + 1)), firstQuoteType) + firstQuoteIndex + 1;
+    const endOfFirstQuoteType = string[endOfFirstQuoteIndex - firstQuoteIndex]
+    const endOfFirstQuote = {
+      endOfFirstQuoteIndex: endOfFirstQuoteIndex,
+      endOfFirstQuoteType: endOfFirstQuoteType
+    }
+    console.log(`endOfFirstQuote`, endOfFirstQuote);
+  }
 };
 check.arr.push("hello");
 
 input.addEventListener("input", async (e) => {
-    const inputJS = e.target.value;
-    const arr = inputJS.replace(/ {2,}/g, " ").split(" ").map(word => check.addSpace(word));
-    arr.forEach(word => {
-        outputEl.textContent += word;
-    })
+  const inputJS = e.target.value;
+  const arr = check.string(inputJS)
+  // const arr = inputJS.replace(/ {2,}/g, " ").split("\n").map(word => word.split(" ")).flat().map(word => check.addSpace(word));
+  // arr.forEach(word => {
+  //   outputEl.textContent += word;
+  // })
+  // console.log(arr);
 })
 
+function getFirstQuoteFirstIndex(string) { return Math.min(...["'", '"', '`'].map(q => string.indexOf(q) >= 0 ? string.indexOf(q) : string.length)) }
